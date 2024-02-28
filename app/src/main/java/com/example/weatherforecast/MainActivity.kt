62602a69909efe7251b92cd24865faa14afa3eec
Samuel Benediktus Meliala <brunodargo.bella@gmail.com>
2024-02-28 17:50:28 +0700

    xml done

---
+++ app/src/main/java/com/example/weatherforecast/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.weatherforecast
+
+import android.os.AsyncTask
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.RelativeLayout
+
+class MainActivity : AppCompatActivity() {
+
+    val CITY : String = "dhaka,bd"
+    val API : String = "8eb8d14c8b8292c621c03fcf95ce7534"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        weatherTask().execute()
+    }
+
+    inner class weatherTask() : AsyncTask<String, Void, String>() {
+        override fun onPreExecute() {
+            super.onPreExecute()
+            findViewById<ProgressBar>(R.id.loader).visibility = View.VISIBLE
+            findViewById<RelativeLayout>()
+        }
+    }
+}
